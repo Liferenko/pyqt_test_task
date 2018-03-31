f488be80f4e05b817bed68a5fee37f68a66916d6
Лиференко Павел <liferenko@reg.ru>
2018-03-31 09:11:32 +0300

    Add some matedata to module

--- work/show_callbacks_results.py
+++ work/show_callbacks_results.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+"""
+
+Mainteiner: Pavel Liferenko (https://t.me/Liferenko)
+
+Description:
+This module collect SQL callbacks and show results in small window. 
+
+Project progress you can see here - https://github.com/Liferenko/pyqt_test_task/projects/
+
+Last edited: March 2018
+"""
 
 from PyQt5.QtCore import QDateTime, QTimeZone, Qt
 
