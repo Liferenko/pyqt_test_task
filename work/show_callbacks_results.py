ddfa1663d7a11a3332825dc3a5de35ab9ec36b52
Лиференко Павел <liferenko@reg.ru>
2018-03-31 09:19:00 +0300

    Cuuuute small window works ^_^ TODO add background mode for this module

--- work/show_callbacks_results.py
+++ work/show_callbacks_results.py
@@ -12,8 +12,9 @@ Project progress you can see here - https://github.com/Liferenko/pyqt_test_task/
 
 Last edited: March 2018
 """
-
+import sys
 from PyQt5.QtCore import QDateTime, QTimeZone, Qt
+from PyQt5.QtWidgets import QApplication, QWidget
 
 now = QDateTime.currentDateTime()
 print( "Time zone: {0}".format(now.timeZoneAbbreviation()) )
@@ -22,3 +23,14 @@ if now.isDaylightTime():
 	print( "The date falls inte DST time" )
 else:
 	print( "The date does not fall into DST time" )
+
+if __name__ == '__main__':
+	result_app = QApplication(sys.argv)
+
+	main_window = QWidget()
+	main_window.resize(250, 150)
+	main_window.move(300, 300)
+	main_window.setWindowTitle( "Result of SQL callbacks" )
+	main_window.show()
+
+	sys.exit(result_app.exec_())
