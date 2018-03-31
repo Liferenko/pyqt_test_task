c7e8212ba80395ecfeaf2f24bdc4c6be34da5e6e
Лиференко Павел <liferenko@reg.ru>
2018-03-31 09:41:30 +0300

    OOP-friendly jelly module

--- work/show_callbacks_results.py
+++ work/show_callbacks_results.py
@@ -13,24 +13,24 @@ Project progress you can see here - https://github.com/Liferenko/pyqt_test_task/
 Last edited: March 2018
 """
 import sys
-from PyQt5.QtCore import QDateTime, QTimeZone, Qt
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-now = QDateTime.currentDateTime()
-print( "Time zone: {0}".format(now.timeZoneAbbreviation()) )
-
-if now.isDaylightTime():
-	print( "The date falls inte DST time" )
-else:
-	print( "The date does not fall into DST time" )
+class Result_window(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+	
+	def initUI(self):
+		self.setGeometry(0, 500, 620, 240) #TODO calibrate coordinates
+		self.setWindowTitle( "Result of SQL callbacks" )
+		self.setWindowIcon( QIcon('result.png') )
+		self.show()
+		
 
 if __name__ == '__main__':
 	result_app = QApplication(sys.argv)
-
-	main_window = QWidget()
-	main_window.resize(250, 150)
-	main_window.move(300, 300)
-	main_window.setWindowTitle( "Result of SQL callbacks" )
-	main_window.show()
+	result_window = Result_window()
 
 	sys.exit(result_app.exec_())
+	exit()
